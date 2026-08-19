d99e14d97c45f84635ae70ece27d9afab7ad52f9
Frank Eller <fe@frankeller.de>
2026-08-19 08:29:55 +0200

    added icons and information to create the extension

--- Fjeller.Guido/ExtensionEntrypoint.cs
+++ Fjeller.Guido/ExtensionEntrypoint.cs
@@ -15,9 +15,15 @@ internal class ExtensionEntrypoint : Extension
 		Metadata = new(
 				id: "Fjeller.Guido.bab7ea95-46ff-41e7-9121-acff60c47f56",
 				version: this.ExtensionAssemblyVersion,
-				publisherName: "Publisher name",
+				publisherName: "fjeller",
 				displayName: "Fjeller.Guido",
-				description: "A small extenson that allows me to add guids to the sourcecode without any other tooling" )
+				description: "A small extension that allows you to add guids to the sourcecode without any other tooling" )
+		{
+			License = "LICENSE.txt",
+			MoreInfo = "https://github.com/fjeller/Fjeller.Guido",
+			Icon = "icon.png",
+			Tags = [ "Guid", "GUID", "UUID", "Editor", "Productivity", "Code Generation", "Snippet" ],
+		}
 	};
 
 	/// <inheritdoc />
