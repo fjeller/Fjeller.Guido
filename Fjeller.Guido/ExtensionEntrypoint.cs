91834e19215e56fedf2f6cb8110f0ca8b901819c
Frank Eller <fe@frankeller.de>
2026-08-19 09:57:18 +0200

    explicitely set preview to false

--- Fjeller.Guido/ExtensionEntrypoint.cs
+++ Fjeller.Guido/ExtensionEntrypoint.cs
@@ -14,15 +14,16 @@ internal class ExtensionEntrypoint : Extension
 	{
 		Metadata = new(
 				id: "Fjeller.Guido.bab7ea95-46ff-41e7-9121-acff60c47f56",
-				version: this.ExtensionAssemblyVersion,
-				publisherName: "fjeller",
+				version: new Version( 1, 0, 0 ),
+				publisherName: "Frank Eller",
 				displayName: "Fjeller.Guido",
 				description: "A small extension that allows you to add guids to the sourcecode without any other tooling" )
 		{
 			License = "LICENSE.txt",
 			MoreInfo = "https://github.com/fjeller/Fjeller.Guido",
 			Icon = "icon.png",
-			Tags = [ "Guid", "GUID", "UUID", "Editor", "Productivity", "Code Generation", "Snippet" ],
+			Preview = false,
+			Tags = [ "Guid", "GUID", "UUID", "Editor", "Productivity", "CodeGeneration", "Snippet" ],
 		}
 	};
 
